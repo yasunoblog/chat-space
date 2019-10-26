7c38acc33887c82652eccdff2991ad7023408106
YourName <you@example.com>
2019-10-26 19:09:26 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # devise_for :users
   root 'messages#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  # resources :users, only: [:index, :edit, :update]
+  # resources :groups, only: [:new, :create, :edit, :update]
 end
